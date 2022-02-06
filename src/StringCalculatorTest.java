6b9375ea8ce7739710c5ebd6e460028505ede6ff
Bindu Devraj <bindudevraj87@gmail.com>
2022-02-06 22:04:59 +0530

    Add test case for empty string input

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -4,5 +4,15 @@ import org.junit.Before;
 
 
 public class StringCalculatorTest {
+	private StringCalculator calc;
 
+	@Before
+	public void init() {
+		calc = new StringCalculator();
+	}
+
+	@Test 
+	public void returnsZeroForEmptyString() {
+		assertEquals(calc.Add(""), 0);
+	}
 }
