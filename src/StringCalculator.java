2e4934782c679f2a92493cecab2ad6cab8b33b24
Bindu Devraj <bindudevraj87@gmail.com>
2022-02-07 23:20:05 +0530

    Refactor test case for empty string, single character, two digit number and sum of two numbers

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,33 +1,28 @@
 public class StringCalculator {
 	public int Add(String numbers) {
-		if(numbers == "" ) {
+		if(isEmpty(numbers)) {
 			return 0;
 		}
 		if(!numbers.contains(",")) {
-			int a = Integer.parseInt(numbers);
-			return a;
+			return stringToIntTransform(numbers);
 		}
 		else if(numbers.contains(",")){
 			String[] input = numbers.split(",");
-			int sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
-			return sum;
+			return Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
 		}
 		return -1;
 	}
+	
+	private boolean isEmpty(String numbers) {
+		  return numbers.isEmpty();
+	}
+	
+	private int stringToIntTransform(String numbers) {
+		  return Integer.parseInt(numbers);
+    }
 }
-//		else if(numbers != searchvalue ) {
-//			int a = Integer.parseInt(numbers);
-//			return a;
-//		}
-//		else {
-////			if(numbers == searchvalue){
-//				String[] input = numbers.split(",");
-//				int sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
-//				return sum;
-////			}
-//		}
-//	}
-//}
+
+
 //	private static int SumofNumbers(String[] input, int size){
 //		for(int i =0 ; i <size; i++) {
 //			int sum = 0;
@@ -35,16 +30,7 @@ public class StringCalculator {
 //			}
 //		return sum;
 //	    }
-//	
-//	private boolean isEmpty(String numbers) {
-//		  return numbers.isEmpty();
-//	  }
-//    
-//    
-//	private int stringToIntTransform(String numbers) {
-//		  return Integer.parseInt(numbers);
-//    }
-//    
-//	private int SumOfTwoNumbers(String numA, String numB) {
-//    	return Integer.parseInt(numA) + Integer.parseInt(numB);
-//    }
+
+//private int SumOfTwoNumbers(String numA, String numB) {
+//	return Integer.parseInt(numA) + Integer.parseInt(numB);
+//}
